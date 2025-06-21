075b3e282cc18f0f1762caaf52f6d4b39855dcc4
Gulfam Zafar <chgulfam@gmail.com>
2025-06-21 22:55:45 +0500

    Use dynamic port for Railway

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express ();
 const authRoutes = require ('./routes/authRoutes');
 const studentRoutes = require('./routes/studentRoutes');
-
-
-require('dotenv').config();
-
 
 app.use(express.json());
 app.use('/api/v1/auth',authRoutes);
@@ -16,11 +13,11 @@ app.get('/api/v1/status',(req ,res)=>{
     res.json({status: 'running'});
 });
 
-mongoose.connect(process.env.MONGO_URI,{
+mongoose.connect(process.env.MONGO_URI ,{
     useNewUrlParser:true,
     useUnifiedTopology:true
 })
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen (PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
 });
