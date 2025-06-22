e30ec08e396ca7a2ffa6381917f28545dda2bd03
Gulfam Zafar <chgulfam@gmail.com>
2025-06-22 22:39:00 +0500

    Add root route to show API live message

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-console.log('MONGO_URI:', process.env.MONGO_URI);
 
 const mongoose = require('mongoose');
 const express = require('express');
@@ -11,14 +9,21 @@ app.use(express.json());
 app.use('/api/v1/auth',authRoutes);
 app.use('/api/v1/students',studentRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Student Record System API is live 🚀');
+});
+
 app.get('/api/v1/status',(req ,res)=>{
     res.json({status: 'running'});
 });
 
-mongoose.connect(process.env.MONGO_URI ,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-})
+require('dotenv').config();
+console.log('MONGO_URI:', process.env.MONGO_URI); // temporary line for debugging
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 const PORT = process.env.PORT || 3000;
 app.listen (PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
